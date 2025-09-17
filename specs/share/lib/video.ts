ce030ee906835fd17028c31d440fee40bd645c3b
Akihiro Yokota <consolas.12@gmail.com>
2025-09-17 18:51:01 +0900

    動画を記録できるようにした

---
+++ specs/share/lib/video.ts
@@ -0,0 +1,48 @@
+import { Page } from "@playwright/test";
+import { ExecutionContext, Gauge } from "gauge-ts";
+import * as path from "path";
+
+async function saveVideo(page: Page, executionContext: ExecutionContext): Promise<void> {
+    const fileName = executionContext.getCurrentScenario().getName().replace(/\s+/g, '-');
+    const filePath = path.join('reports', 'playwright-report', 'videos', `${fileName}.webm`);
+
+    await page.video().saveAs(filePath);
+    // もともと作られていたランダムな文字列の名前の動画ファイルは削除しておく
+    await page.video().delete();
+
+    Gauge.writeMessage(`Video has been saved. To inspect, run:\n&nbsp;&nbsp;&gt;&nbsp;open &quot;${filePath}&quot;&nbsp;`);
+}
+
+async function discardVideo(page: Page): Promise<void> {
+    await page.video().delete();
+}
+
+export async function recordVideo(page: Page, executionContext: ExecutionContext): Promise<void> {
+    const recordVideo = process.env.record_video;
+    const isScenarioFailed = executionContext.getCurrentScenario().getIsFailing();
+
+    // 設定なし、もしくはoffの場合は何もしない
+    if (!recordVideo || recordVideo === 'off') {
+        await discardVideo(page);
+        return;
+    }
+
+    // onの場合は常に動画を保存
+    if (recordVideo === 'on') {
+        await saveVideo(page, executionContext);
+        return;
+    }
+
+    // retain-on-failureの場合は、シナリオが失敗した場合のみ動画を保存
+    if (recordVideo === 'retain-on-failure') {
+        if (isScenarioFailed) {
+            await saveVideo(page, executionContext);
+        } else {
+            await discardVideo(page);
+        }
+        return;
+    }
+
+    // その他の値の場合はエラーを投げる
+    throw new Error(`Invalid value for recordVideo: ${recordVideo}. Valid values are 'on', 'off', 'retain-on-failure'.`);
+}
