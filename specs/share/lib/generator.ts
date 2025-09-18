69ad633450ada6d70cecde3909a084c3851be6cb
Akihiro Yokota <consolas.12@gmail.com>
2025-09-18 18:56:17 +0900

    generateUniqueString関数を拡張した

--- specs/share/lib/generator.ts
+++ specs/share/lib/generator.ts
@@ -1,9 +1,14 @@
 import dayjs from "dayjs";
 
-async function generateUniqueString(prefix: string = ""): Promise<string> {
+type GenerateUniqueStringArgs = {
+    prefix?: string;
+    suffix?: string;
+};
+
+function generateUniqueString(args: GenerateUniqueStringArgs = {}): string {
     const timestamp = dayjs().format("YYYY-MM-DD-HH-mm-ss");
     const randomText = Math.random().toString(36).substring(2, 8);
-    return `${prefix ? `${prefix}-` : ""}${randomText}-${timestamp}`;
+    return `${args.prefix ? `${args.prefix}-` : ""}${randomText}-${timestamp}${args.suffix ? `-${args.suffix}` : ""}`;
 }
 
 export const Generator = {
